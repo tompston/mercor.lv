0338074a44ac731d77e0f17826d5446a20ded48b
tompston <tompston@protonmail.com>
2022-01-20 14:55:39 +0200

    added img, minor tweaks to css

--- js/gsap_animations.js
+++ js/gsap_animations.js
@@ -9,16 +9,25 @@ gsap.from('.main_header_text', {
 })
 
 // animations for the elements in the image grid with delay
-const third_img_grid = ['.th_img_1', '.th_img_2', '.th_img_3', '.th_img_4']
+const third_img_grid = [
+  '.th_img_1',
+  '.th_img_2',
+  '.th_img_3',
+  '.th_img_4',
+  '.th_img_5',
+  '.th_img_6',
+  '.th_img_7',
+  '.th_img_8',
+]
 
 for (let i = 0; i < third_img_grid.length; i++) {
   let element = third_img_grid[i]
 
   gsap.from(element, {
-    duration: 0.9,
+    duration: 0.6,
     opacity: 0,
-    x: 50,
-    delay: i / 6,
+    x: 30,
+    delay: i / 10,
     ease: 'power4.out',
     scrollTrigger: {
       trigger: '.third_img',
